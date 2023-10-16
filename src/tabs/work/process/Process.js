5962dc230e3b1120cad5b0e4221d9a8f0bffccdc
Yonathan Dessalene <ydessalene@gmail.com>
2023-10-15 20:53:59 -0400

    Process, Services: Roof replacement done

--- src/tabs/work/process/Process.js
+++ src/tabs/work/process/Process.js
@@ -1,5 +1,113 @@
+import './Process.css';
+import '../../../App.css';
+import base64Images from '../../../assets/base64-images.config.json';
+import halfLogo from '../../../assets/work/half-logo.png';
+import inspect from '../../../assets/work/inspect.png';
+
 const Process = () => {
-    return <h1>Process</h1>;
+    const imageMap = base64Images;
+    
+    return <>
+        <div class="our-work-container">
+            <div class="work-banner">
+                <div class="work-banner-text-container">
+                    <p class="banner-text-small"> OUR PROCESS </p>
+                    <p class="banner-text-large"> Simplified for Exceptional Service </p>
+                </div><img class="banner-icon" src={imageMap.pointUp} alt="Banner Up" />
+            </div>
+            <div class="work-process-container">
+                <div class="work-process-banner">
+                    <p class="main-text"> OUR FOUR STEP PROCESS </p>
+                    <p class="small-text"> A standard approach to every job ensures a quality restoration </p>
+                </div>
+                <div class="work-process-content">
+                    <div class="work-item-container">
+                        <div class="work-item-icon-container">
+                            <img src={inspect} alt="Inspect" />
+                        </div>
+                        <div class="work-item-info-container">
+                            <p class="step-header"> 1. Inspect </p>
+                            <p class="step-info"> ROOF<span class="logo-text">ER</span> will perform a FREE inspection of your roof, siding, and gutters to find evidence of wind, hail, or other damage. </p>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> You will be notified once we arrive (no direct contact required) </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> We will take all necessary safety precautions prior to performing the inspection </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Next, we'll complete our free inspection using our custom Field - App to determine the condition of your exterior systems </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work-item-container">
+                        <div class="work-item-icon-container">
+                            <img src={imageMap.diagnose} alt="Diagnose" />
+                        </div>
+                        <div class="work-item-info-container">
+                            <p class="step-header"> 2. Diagnose </p>
+                            <p class="step-info"> ROOF<span class="logo-text">ER</span> and your Roof Doc will properly assess all findings to determine whether your exterior systems are storm-damaged, old, or perfectly fine. </p>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Our Field-app goes through a step-by-step checklist to ensure we gather all the information needed to properly assess your roof, siding, &amp; gutter systems  </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> The items we look for include wind and hail damage, weathering effects, faulty installation methods, etc. </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Upon synthesizing our findings, we will determine our recommended course of action: a repair or replacement, an insurance claim for storm damage, or a simple sign off that your exterior is in good shape for a few more years! </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work-item-container">
+                        <div class="work-item-icon-container">
+                            <img src={imageMap.prescribe} alt="Prescribe" />
+                        </div><div class="work-item-info-container">
+                            <p class="step-header"> 3. Prescribe </p>
+                            <p class="step-info"> Your Roof Doc will provide you with an honest recommendation based on their findings. </p>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Property insurance claim: if ROOF<span class="logo-text">ER</span> finds storm-related damage to your roof, siding, or gutters, we may recommend filing a claim with your insurance company. You can learn more about this process by visiting our <a href="/the-roof-docs/#/faqs"><span class="roofDocsRed">FAQs page</span> </a> </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Repair/replacement: often times, homeowners ask us to inspect their property because they're aware of an issue already. In these cases, our Roof Docs are experts at deciding exactly what needs done to ensure those problems are addressed </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Good/great condition: even if your roof/siding/gutters were installed quite a few years ago, there's a chance they have maintained their useful life and will remain serviceable for a few more years. We pride ourselves on providing honest recommendations to our homeowners - we won't sell you what you don't need! </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="work-item-container">
+                        <div class="work-item-icon-container">
+                            <img src={imageMap.operate} alt="Operate" />
+                        </div>
+                        <div class="work-item-info-container">
+                            <p class="step-header"> 4. Operate </p>
+                            <p class="step-info"> If your roof, siding, or gutters are found to have an ailment, the last step is to review project details with your Roof Doc and let ROOF<span class="logo-text">ER</span>'s install team take care of the rest.  </p>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> If work is required, we will do our best to educate you on all of your options, focusing on how to provide you with the most value </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Our Roof Docs can recommend colors &amp; products based on your style of home and your preferences. The best part? Our pricing is 100% haggle-free </p>
+                            </div>
+                            <div class="work-step-item-container">
+                                <img alt="icon" src={halfLogo} />
+                                <p> Once the project details have been completed, our production team takes over and our factory-certified installers deliver a professional remodeling project! </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
 };
-  
+
 export default Process;
--- src/tabs/work/roof-repair/RoofRepair.js
+++ src/tabs/work/roof-repair/RoofRepair.js
@@ -1,5 +1,54 @@
+import './RoofRepair.css';
+import '../../../App.css';
+import base64Images from '../../../assets/base64-images.config.json';
+
 const RoofRepair = () => {
-    return <h1>RoofRepair</h1>;
+    const imageMap = base64Images;
+
+    return <>
+        <div class="services-container">
+            <div class="services-banner">
+                <div class="services-banner-text-container">
+                    <p class="banner-text-small"> Professional Roof Repair Services Near You in VA &amp; MD </p>
+                    <p class="banner-text-large"> Roof-ER are your Fairfax roof repair experts</p>
+                </div>
+                <img class="banner-icon" src={imageMap.pointUp} alt="Banner Icon Up" />
+            </div>
+            <div class="services-template-container">
+                <div class="services-template-content">
+                    <h1>Roof Repair Services</h1>
+                    <p class="main-text">When your roof gets damaged, you need trusted roof repair professionals to diagnose and fix the damage before it turns into a bigger, more expensive issue. At Roof-ER, we specialize in roof repairs in Northern Virginia and Maryland. Our certified and expertly-trained professional roofers will restore your roof to its original quality with a fast turnaround — and we even provide <a href="/the-roof-docs/#/inspection">free contactless estimates</a> for every customer.</p>
+                    <h2>Why Do I Need to Repair My Roof?</h2>
+                    <p>Whether a bad storm rolled through your area or water has been pooling on your roof for days, you should never ignore emerging roof issues. Not only can ignored roof damage worsen into a costly problem for your home, but it can also pose a safety risk and decrease your property value.</p>
+                    <p>You need to have your roof damage repaired as soon as possible by professional roofing contractors who specialize in your type of roof. The sooner you address your roofing issues, the less you'll have to spend on future repairs and maintenance. Our roof repair services are top-rated in Fairfax, VA and the surrounding communitites.</p>
+                    <h2>Common Roof Problems</h2>
+                    <p>Here are some of the most common roof problems that our&nbsp;<a title="" href="/the-roof-docs/#/mission" target="roof repair company">roof repair company</a>&nbsp;in Tysons can address:</p>
+                    <h3>Roof Leaks</h3>
+                    <p>Roof leaks are common issues in Northern VA. They typically occur after periods of heavy rain, and they indicate that your roof has:</p>
+                    <ul>
+                        <li>Cracked or otherwise damaged flashing</li>
+                        <li>Broken or torn shingles or tiles</li>
+                        <li>Damaged skylights</li>
+                        <li>Chimney cracks</li>
+                        <li>Improperly sealed roof penetrations</li>
+                        <li>Carpenter gaps in the roof decking</li>
+                    </ul>
+                    <p>There are many causes for roof leaks, and the location of the leaks often indicates where your roof may be damaged.</p>
+                    <h3>Pooled Water</h3>
+                    <p>Pooled water occurs after a rainstorm and can result in water damage to your roof if it's left untreated. However, tapering your roof's slope will prevent pools of water from forming. If you've had water pooling on your roof for multiple days or weeks, you need to have your roof inspected for water damage before any tapering can be completed.</p>
+                    <h3>Shrinkage</h3>
+                    <p>Shrinkage happens when your roofing materials begin to deteriorate, warp or crack as a result of aging. When shrinkage occurs, you need to have the issue addressed as soon as possible.</p>
+                    <h3>Hail Damage</h3>
+                    <p>Even,&nbsp;<a title="" href="/the-roof-docs/#/faqs" target="places like Northern VA experience hail">places like Northern VA experience hail</a>. When hailstorms occur, they tend to cause varying degrees of damage to roofs. Hail damage can be difficult to assess but, if present, can lead to costly repairs. It is best to consult a professional roof inspector if you believe your home has been <a href="/the-roof-docs/#/blog/hail-damage">impacted by hail</a>.</p>
+                    <h3>Storm Damage (Wind)</h3>
+                    <p>Strong storms are also common reasons for roof repair service in the Tysons area. When your roof is exposed to strong gusts of wind, your shingles blow off, leaving your roof vulnerable to more serious damage. Storms can also pummel your roof with debris, which can prove quite destructive to most roof types.</p>
+                    <p>If you experience any of the roofing issues listed above, it's time to call The Roof Docs at Roof-ER for fast roof repair services in Fairfax County.</p>
+                    <h2>Roof Repair Service in Fairfax County</h2>
+                    <p>When you need a top-notch roof repair contractor in Vienna or the surrounding communities, turn to The Roof Docs. <a href="/the-roof-docs/#/inspection">Schedule your free contactless estimate for roof repair</a>&nbsp;when you call us today at <a href="7032393738"> 703-239-3738</a>!</p>
+                </div>
+            </div>
+        </div>
+    </>;
 };
-  
+
 export default RoofRepair;
